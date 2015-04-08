95ddb276b59eb4dedb2771ac8e21d5bf5d3198a3
Tom Rochette <roctom@gmail.com>
2015-04-08 10:12:40 -0400

    [#1] Initial shell adapter implementation.

--- bin/hack-bot.php
+++ bin/hack-bot.php
@@ -1,18 +1,13 @@
 <?hh // strict
 
 use tomzx\HackBot\Core\Dispatcher;
-use tomzx\HackBot\Core\Request;
+use tomzx\HackBot\Adapters\Shell;
 
 require 'vendor/autoload.php';
 
 $dispatcher = new Dispatcher();
 $dispatcher->initializeResponders();
 
-$query = implode(' ', array_slice($argv, 1));
-$request = new Request();
-$request->setRequest($query);
-
-$response = $dispatcher->dispatch($request);
-echo 'QUERY WAS: '.$query.PHP_EOL;
-var_dump($response->hasResponse());
-var_dump($response->getResponse());
+$shell = new Shell();
+$shell->setDispatcher($dispatcher);
+$shell->run();
--- src/tomzx/HackBot/Adapters/Shell.php
+++ src/tomzx/HackBot/Adapters/Shell.php
@@ -2,7 +2,35 @@
 
 namespace tomzx\HackBot\Adapters;
 
+use tomzx\HackBot\Core\Dispatcher;
+use tomzx\HackBot\Core\Request;
+
 class Shell
 {
+	protected Dispatcher $dispatcher = null;
 
+	public function setDispatcher(Dispatcher $dispatcher) : this
+	{
+		$this->dispatcher = $dispatcher;
+
+		return $this;
+	}
+
+	public function run()
+	{
+		while(true) {
+			$query = trim(fgets(STDIN));
+			if ($query === 'exit') {
+				break;
+			}
+
+			$request = new Request();
+			$request->setRequest($query);
+
+			$response = $this->dispatcher->dispatch($request);
+			echo 'QUERY WAS: '.$query.PHP_EOL;
+			var_dump($response->hasResponse());
+			var_dump($response->getResponse());
+		}
+	}
 }
