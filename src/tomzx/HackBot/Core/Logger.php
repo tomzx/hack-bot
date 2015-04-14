f9f30f05d85ff3ce8ea3cbf39a8402ead6200678
Tom Rochette <roctom@gmail.com>
2015-04-13 22:02:23 -0400

    Add many logging levels to the Logger.

--- src/tomzx/HackBot/Core/Logger.php
+++ src/tomzx/HackBot/Core/Logger.php
@@ -18,11 +18,56 @@ class Logger
 		self::$enabled = $enabled;
 	}
 
-	public static function log(string $text) : void
+	public static function debug(string $text) : void
+	{
+		self::logLevel('DEBUG', $text);
+	}
+
+	public static function info(string $text) : void
+	{
+		self::logLevel('INFO', $text);
+	}
+
+	public static function notice(string $text) : void
+	{
+		self::logLevel('NOTICE', $text);
+	}
+
+	public static function warning(string $text) : void
+	{
+		self::logLevel('WARNING', $text);
+	}
+
+	public static function error(string $text) : void
+	{
+		self::logLevel('ERROR', $text);
+	}
+
+	public static function critical(string $text) : void
+	{
+		self::logLevel('CRITICAL', $text);
+	}
+
+	public static function alert(string $text) : void
+	{
+		self::logLevel('ALERT', $text);
+	}
+
+	public static function emergency(string $text) : void
+	{
+		self::logLevel('EMERGENCY', $text);
+	}
+
+	protected static function logLevel(string $level, string $text) : void
+	{
+		self::log('['.$level.'] '.$text);
+	}
+
+	protected static function log(string $text) : void
 	{
 		if ( ! self::$enabled) {
 			return;
 		}
-		echo '['.date('c').']['.Helper::formatBytes(memory_get_usage()).'] '.$text.PHP_EOL;
+		echo '['.date('c').']['.Helper::formatBytes(memory_get_usage()).']'.$text.PHP_EOL;
 	}
 }
