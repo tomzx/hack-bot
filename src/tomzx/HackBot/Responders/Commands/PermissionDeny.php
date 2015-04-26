4b3273c8d3bb59e8c345b5373d4164ea38b1b9dc
Tom Rochette <roctom@gmail.com>
2015-04-25 20:50:20 -0400

    Add permission allow/deny commands.

---
+++ src/tomzx/HackBot/Responders/Commands/PermissionDeny.php
@@ -0,0 +1,45 @@
+<?hh // strict
+
+namespace tomzx\HackBot\Responders\Commands;
+
+use Arry\Arry;
+use tomzx\HackBot\Adapters\IRC;
+use tomzx\HackBot\Core\Dispatcher;
+use tomzx\HackBot\Core\User;
+use tomzx\HackBot\Core\Responders\Command;
+
+return [
+	'type' => 'command',
+	'identifier' => 'permission-deny',
+	'command' => 'permission-deny',
+	'parameters' => '(?<user>\S+) (?<permissions>.*)',
+	'help' => 'user permission [permissions ...]',
+	'answer' => function (Dispatcher $dispatcher, array $data = [])
+	{
+		// TODO: Only allow logged-in/server authenticated users to give users permissions
+		$adapter = Arry::get($data, 'meta.irc.adapter');
+		if ( ! $adapter) {
+			return;
+		}
+
+		$from = Arry::get($data, 'meta.irc.nick');
+		$user = Arry::get($data, 'user');
+		$permissions = explode(' ', Arry::get($data, 'permissions'));
+
+		$lockManager = $dispatcher->getLockManager();
+
+		$fromLock = $lockManager->caller(new User($from));
+
+		if ( ! $fromLock->can('permissions.write')) {
+			return 'You are not allowed to edit permissions.';
+		}
+
+		$userLock = $lockManager->caller(new User($user));
+
+		foreach ($permissions as $permission) {
+			$userLock->deny($permission);
+		}
+
+		return 'The following permissions were removed from user '.$user.': '.implode(', ', $permissions);
+	}
+];
